a53e22f854d31850a28670a16025f54571b86579
Feliphe Mickael Pereira <feliphemickael@proton.me>
2023-03-31 18:34:53 -0300

    Adicionando primeiros exercícios

---
+++ README.md
@@ -0,0 +1,6 @@
+# dio-loops-e-arrays
+Repositório para exercícios do curso "Estruturas de Repetição e Arrays em Java" da DIO.
+
+## Links Úteis
+
+[Exercícios](https://wiki.python.org.br/EstruturaDeRepeticao)
---
+++ src/edu/feliphe/exercicios/arrays/Ex1_OrdemInversa.java
@@ -0,0 +1,25 @@
+package edu.feliphe.exercicios.arrays;
+
+/*
+Crie um vetor de 6 números inteiros
+e mostre-os na ordem inversa.
+*/
+public class Ex1_OrdemInversa {
+    public static void main(String[] args) {
+
+        int[] vetor = {-10, 100, 11, 9, 2, -90};
+
+        System.out.print("Vetor: ");
+        int count = 0;
+        while(count < (vetor.length)) {
+            System.out.print(vetor[count] + " ");
+            count++;
+        }
+
+        System.out.print("\nVetor: ");
+        for(int i = (vetor.length - 1); i >= 0; i --) {
+            System.out.print(vetor[i] + " ");
+        }
+
+    }
+}
